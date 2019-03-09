376b9d5755a016eb0336f2fa229a7001792e9e5a
david-chua <dp_chua@hotmail.com>
2019-03-09 13:47:33 -0500

    edit completed

--- data/controllers/actions.js
+++ data/controllers/actions.js
@@ -49,4 +49,40 @@ server.post('/', (req,res) => {
     });
 });
 
+server.delete('/:id',(req,res) =>{
+  const {id} = req.params;
+  actions
+    .remove(id)
+    .then(response => {
+      if (response === 0){
+        return errorHelper(404, 'No projects found with that ID', res);
+      }
+      res.json(response)
+    })
+    .catch(err => {
+      return errorHelper(500, "Internal server error", res);
+    });
+})
+
+server.put('/:id', (req,res) =>{
+  const {id} = req.params;
+  const updatedAction = {
+    project_id: req.body.project_id,
+    description: req.body.description,
+    notes: req.body.notes,
+    completed: req.body.completed
+  }
+  actions
+    .update(id, updatedAction)
+    .then(response => {
+      if (response === 0){
+        return errorHelper(404, 'No projects found with that ID', res);
+      }
+      res.json(response)
+    })
+    .catch(err => {
+      return errorHelper(500, "Internal server error", res);
+    });
+})
+
 module.exports = server;
