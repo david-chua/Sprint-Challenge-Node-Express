75c6e242f37cc5900844ffa44fc6b23eb3cb097a
david-chua <dp_chua@hotmail.com>
2019-03-09 13:18:40 -0500

    POST working

--- data/controllers/projects.js
+++ data/controllers/projects.js
@@ -1,8 +1,36 @@
 const express = require('express');
 
-const project = require('../helpers/projectModel.js');
+const projects = require('../helpers/projectModel.js');
 
 const server = express.Router();
+
+server.get('/', (req,res) => {
+  projects
+    .get()
+    .then(foundProjects =>{
+      res.json(foundProjects);
+    })
+    .catch(err => {
+      return errorHelper(500, 'Internal server error', res);
+    })
+})
+
+server.post('/', (req,res) => {
+  const newProject = {
+    name: req.body.name,
+    description: req.body.description,
+    completed: req.body.completed
+  }
+  projects
+    .insert(newProject)
+    .then(response => {
+      console.log(response)
+      res.json(response)
+    })
+    .catch(err => {
+      return errorHelper(500, 'Internal server error', res);
+    });
+});
 
 
 
